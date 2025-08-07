d1dbd7918fc8ce3cdefd6fd0fa9ff9b76f5e89cf
knowindiatravel <knowindiatravel5@gmail.com>
2025-08-07 21:04:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,24 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
-import { resolve } from "path";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// Load env vars from .env file
-dotenv.config();
+export default ({ mode }) => {
+  // Load env variables based on mode (development/production)
+  const env = loadEnv(mode, process.cwd(), '');
 
-// Access the environment variable
-const API_BASE_URL = process.env.VITE_API_BASE_URL;
-
-export default defineConfig({
-  plugins: [react()],
-  assetsInclude: ["**/*.mp4"],
-  base: "/",
-  build: {
-    outDir: "../backend/client/dist",
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: API_BASE_URL,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
+  return defineConfig({
+    plugins: [react()],
+    define: {
+      // Explicitly define environment variables
+      'process.env': env
     },
-  },
-});
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL || 'http://localhost:8080',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
+    }
+  });
+};
